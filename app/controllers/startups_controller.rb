d94c2a9f289f78109e40c432844a2fcaf75f2e64
David Chouinard <david.chouinard@me.com>
2012-11-08 15:17:21 -0500

    Pinging is now persistent

--- app/controllers/startups_controller.rb
+++ app/controllers/startups_controller.rb
@@ -4,6 +4,10 @@ class StartupsController < ApplicationController
   def index
     @startups = Startup.all
 
+    if user_signed_in?
+      @pings = UserPing.where(:user_id => current_user.id)
+    end
+
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @startups }
@@ -14,77 +18,16 @@ class StartupsController < ApplicationController
     @startup = Startup.find(params[:id])
 
     if user_signed_in?
-      puts "TESTING"
-    else
-      puts "logged out"
-    end
-
-    # Make sure the user hasn't already pinged that startup
-    unless UserPing.where(:startup_id => @startup.id, :user_id => current_user.id)
-      if UserPing.create(:startup_id => @startup.id, :user_id => current_user.id)
-        PingMailer.ping_startup(@startup).deliver
+      # Make sure the user hasn't already pinged that startup
+      unless UserPing.exists?(:startup_id => @startup.id, :user_id => current_user.id)
+        if UserPing.create(:startup_id => @startup.id, :user_id => current_user.id)
+          PingMailer.ping_startup(@startup).deliver
+        end
       end
+    else
+      response.status = 403
     end
 
     render :nothing => true
-  end
-
-  # GET /startups/1
-  # GET /startups/1.json
-  def show
-    @startup = Startup.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @startup }
-    end
-  end
-
-  # GET /startups/new
-  # GET /startups/new.json
-  def new
-    @startup = Startup.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @startup }
-    end
-  end
-
-  # GET /startups/1/edit
-  def edit
-    @startup = Startup.find(params[:id])
-  end
-
-  # POST /startups
-  # POST /startups.json
-  def create
-    @startup = Startup.new(params[:startup])
-
-    respond_to do |format|
-      if @startup.save
-        format.html { redirect_to @startup, notice: 'Startup was successfully created.' }
-        format.json { render json: @startup, status: :created, location: @startup }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @startup.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /startups/1
-  # PUT /startups/1.json
-  def update
-    @startup = Startup.find(params[:id])
-
-    respond_to do |format|
-      if @startup.update_attributes(params[:startup])
-        format.html { redirect_to @startup, notice: 'Startup was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @startup.errors, status: :unprocessable_entity }
-      end
-    end
   end
 end
